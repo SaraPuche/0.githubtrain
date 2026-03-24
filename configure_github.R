2f87dde4359c7c61b23af87eaba1e70904a5dc5f
SaraPuche <sara.puche@uclm.es>
2026-03-24 11:12:42 +0100

    R script with information about Github configuration

---
+++ configure_github.R
@@ -0,0 +1,12 @@
+# Configure GIT
+
+# We have to tell GIT who we are
+
+library(usethis)
+use_git_config(user.name= "SaraPuche", user.email= "sara.puche@uclm.es")
+
+# if I currently working in a project:
+library(usethis)
+use_git()
+
+# restart Rstudio
